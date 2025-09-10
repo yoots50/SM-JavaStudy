0ae1a46aea4cb4e70401855d176be35b0e85f26e
yoots50 <202210947@sangmyung.kr>
2025-09-10 13:18:32 +0900

    오타 수정

--- java_test/src/ch01/sec08/Hello.java
+++ java_test/src/ch01/sec08/Hello.java
@@ -3,7 +3,7 @@ package ch01.sec08;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, java");
+		System.out.println("Hello, Java");
 	}
 
 }
--- java_test/src/ch02/sec03/CharExample.java
+++ java_test/src/ch02/sec03/CharExample.java
@@ -1,4 +1,4 @@
-package ch02.sec03;
+	package ch02.sec03;
 
 public class CharExample {
 
