0d43bb601d2ea14b33834f0c67b8b84fc699746d
yoots50 <202210947@sangmyung.kr>
2025-09-28 22:16:59 +0900

    week 4

---
+++ java_test/src/ch04/sec03/SwitchExample.java
@@ -0,0 +1,24 @@
+package ch04.sec03;
+
+public class SwitchExample {
+
+	public static void main(String[] args) {
+		int num = (int)(Math.random() * 6) + 1;
+		
+		switch (num) {
+		case 1: 
+			System.out.println("1번이 나왔습니다.");
+		case 2:
+			System.out.println("2번이 나왔습니다.");
+		case 3:
+			System.out.println("3번이 나왔습니다.");
+		case 4:
+			System.out.println("4번이 나왔습니다.");
+		case 5:
+			System.out.println("5번이 나왔습니다.");
+		case 6:
+			System.out.println("6번이 나왔습니다.");
+		}
+	}
+
+}
